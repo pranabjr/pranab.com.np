bcee5d31ae7cc4512ab385ffb29cdcab1e68a365
Pranab Jung Rana <pranabjr@gmail.com>
2016-04-14 23:16:54 +0200

    download cv button added

--- js/parallax.js
+++ js/parallax.js
@@ -8,11 +8,11 @@ $(window).scroll(function(){
     });
     
     $('.logo').css({
-      'transform' : 'translate(0px, '+ wScroll /30 +'%)'
+      'transform' : 'translate(0px, '+ wScroll /20 +'%)'
     });
     
     $('.sicons').css({
-      'transform' : 'translate(0px, '+ wScroll /30 +'%)'
+      'transform' : 'translate(0px, '+ wScroll /20 +'%)'
     });
 
     $('.back-box').css({
