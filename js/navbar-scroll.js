f913b9d9277d9a19baf99657d55f046b7c686817
Pranab Jung Rana <pranabjr@gmail.com>
2016-04-14 14:43:28 +0200

    files added

---
+++ js/navbar-scroll.js
@@ -0,0 +1,11 @@
+var  mn = $(".main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('.main-box').height()-5;
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
--- js/parallax.js
+++ js/parallax.js
@@ -4,7 +4,19 @@ $(window).scroll(function(){
 
 
     $('.fore-box').css({
-      'transform' : 'translate(0px, -'+ wScroll /20 +'%)'
+      'transform' : 'translate(0px, -'+ wScroll /21 +'%)'
+    });
+    
+    $('.logo').css({
+      'transform' : 'translate(0px, '+ wScroll /30 +'%)'
+    });
+    
+    $('.sicons').css({
+      'transform' : 'translate(0px, '+ wScroll /30 +'%)'
+    });
+
+    $('.back-box').css({
+      'transform' : 'translate(0px, '+ wScroll /10 +'%)'
     });
 
   });
